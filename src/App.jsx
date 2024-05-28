f91d436a9d2ff1865edf85db8038fb90a107b412
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-28 19:03:48 +0530

    tailwind-practice

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+<div className="grid grid-cols-3 gap-4">
+<div className='bg-black text-white'>01</div>
+<div className='bg-black text-white'>01</div>
+<div className='bg-black text-white'>01</div>
+</div>
+  )
+}
+
+export default App
