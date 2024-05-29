e0970b92661766f3f77963b3a97156c7b44fd57b
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-29 15:52:10 +0530

    main

---
+++ src/components/RevenueCard.jsx
@@ -0,0 +1,20 @@
+
+export const RevenueCard = () => {
+    return <div className="shadow-lg rounded bg-white p-4">
+        <div className="text-gray-600 justify-start flex">
+       Amount Pending
+        </div>
+        <div className="flex justify-between">
+            <div>
+            ₹ 92,087
+            </div>
+         
+
+          <div className="flex ">
+            13 Orders
+          </div>
+        </div>
+    
+   
+    </div>
+}
