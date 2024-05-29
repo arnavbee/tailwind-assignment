3e53f7f25c37915cb31a5c50986318599bff5e69
Arnav <105158586+arnavbee@users.noreply.github.com>
2024-05-29 13:46:06 +0530

    main

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,32 @@ import './App.css'
 function App() {
 
   return (
-<div className="flex flex-row gap-4">
-<div className='bg-black text-white w-196'>01</div>
-<div className='bg-black text-white w-200'>01</div>
+<div className="flex flex-row w-full h-full m-0 p-0">
+<div className='bg-[#1e263f] text-white w-2/12 h-screen p-0 m-0'>
+<div className='grid grid-rows-12 gap-7 pt-5'>
+<div>Logo</div>
+
+
+<div className=" bg-gray-500 flex">
+<img src="./home1.svg" className='w-6 h-6 mt-3.5 mr-2 ml-5'  alt="Home" />
+ <p className='mt-4 mr-0'>Home</p>
+</div>
+<div className="bg-black ml-0 p-4" ><p>Orders</p></div>
+<div className="bg-black ml-0 p-4">Products</div>
+<div className="bg-black ml-0 p-4">Delivery</div>
+<div className="bg-black ml-0 p-4">Marketing</div>
+<div className="bg-black ml-0 p-4">Analytics</div>
+<div className="bg-black ml-0 p-4">Payouts</div>
+<div className="bg-black ml-0 p-4">Discounts</div>
+<div className="bg-black ml-0 p-4">Audience</div>
+<div className="bg-black ml-0 p-4">Appearance</div>
+<div className="bg-black ml-0 p-4">Plugins</div>
+
+
+</div>
+
+</div>
+<div className='bg-black text-white w-10/12 h-screen p-0 m-0'>01</div>
 
 </div>
 
